c40c1c17b5791891e0a37935af18bf6e78b3bac7
elkolbo <lorenzkolb.technik@gmail.com>
2024-01-21 18:17:45 +0100

    merge error removed

--- config.py
+++ config.py
@@ -42,35 +42,6 @@ class config:
         self.NUM_ACTIONS = self.WIDTH
         self.STATE_SHAPE = (2, self.HEIGHT, self.WIDTH)
 
-        ###########################
-        ##### Game variables ######
-        ###########################
-
-        # Constants
-        WIDTH, HEIGHT = 7, 6
-        CELL_SIZE = 100
-        # WINDOW_WIDTH, WINDOW_HEIGHT = WIDTH * CELL_SIZE + 2, (HEIGHT + 2.5) * CELL_SIZE +2
-        WINDOW_WIDTH, WINDOW_HEIGHT = (
-            WIDTH * CELL_SIZE + 2 * CELL_SIZE,
-            (HEIGHT + 2.5) * CELL_SIZE + 2,
-        )
-
-        FPS = 30
-
-        # # Colors
-        # BACKGROUND_COLOR = (200, 200, 200)  # Neutral background color
-        # GRID_COLOR = (0, 0, 0)  # Grid color
-        # RED = (255, 0, 0)
-        # YELLOW = (255, 255, 0)
-        # BLUE = (0, 0, 255)
-
-        BACKGROUND_COLOR = (25, 25, 25)  # Dark background color
-        GRID_COLOR = (100, 100, 100)  # Grid color
-        RED = (255, 0, 0)
-        YELLOW = (255, 255, 0)
-        BLUE = (0, 0, 255)
-        GLOW_GREEN = (0, 255, 0)
-
         # Define constants for players
-        HUMAN_PLAYER = 1
-        RL_PLAYER = 2
+        self.HUMAN_PLAYER = 1
+        self.RL_PLAYER = 2
