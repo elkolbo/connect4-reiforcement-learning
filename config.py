103bad1d5a75976e3a012091a37e9c9ddd5673bd
lorenz <lorenz.kolb@tum.de>
2024-01-21 22:03:07 +0100

    continued training

--- config.py
+++ config.py
@@ -4,25 +4,22 @@ class config:
         ##### Agent variables #####
         ###########################
 
-        self.num_episodes = 10000
-        self.learning_rate = 0.0005
+        self.num_episodes = 5000
+        self.learning_rate = 0.001
 
         self.gamma = 0.9  # Discount factor for q-learning
 
         self.train_from_start = False
 
-        self.epsilon_start = 0.5
-        self.epsilon_end = 0.0
-        self.epsilon_decay = 0.9999
-        self.target_update_frequency = 10
+        self.epsilon_start = 0.9
+        self.epsilon_end = 0.1
+        self.epsilon_decay = 0.99
+        self.target_update_frequency = 200
         self.batch_size = 64
 
-        self.visualization_frequency = (
-            1  # Put in a high value to train faster e.g. 10000
-        )
         self.visualization_frequency = 1000  # Put in a high value to train faster
 
-        self.opponent_switch_interval = 5
+        self.opponent_switch_interval = 3
 
         # Visualization constants
         self.WIDTH, self.HEIGHT = 7, 6
